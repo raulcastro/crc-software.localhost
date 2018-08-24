342a4542d8a70f4e2b8b241aa3a5ffaa2203cec1
unknown <gonzalez.cynth12@gmail.com>
2018-08-24 18:35:16 -0500

    php

---
+++ header.php
@@ -0,0 +1,27 @@
+<header class="page-header">
+        <!-- RD Navbar-->
+        <div class="rd-navbar-wrap"> 
+          <nav class="rd-navbar" data-layout="rd-navbar-fixed" data-sm-layout="rd-navbar-fixed" data-sm-device-layout="rd-navbar-fixed" data-md-layout="rd-navbar-fixed" data-md-device-layout="rd-navbar-fixed" data-lg-device-layout="rd-navbar-fixed" data-xl-device-layout="rd-navbar-static" data-xxl-device-layout="rd-navbar-static" data-lg-layout="rd-navbar-fixed" data-xl-layout="rd-navbar-static" data-xxl-layout="rd-navbar-static" data-stick-up-clone="false" data-sm-stick-up="true" data-md-stick-up="true" data-lg-stick-up="true" data-xl-stick-up="true" data-xxl-stick-up="true" data-md-stick-up-offset="69px" data-lg-stick-up-offset="1px" data-body-class="rd-navbar-default-linked">
+            <div class="rd-navbar-inner">
+              <!-- RD Navbar Panel-->
+              <div class="rd-navbar-panel">
+                <button class="rd-navbar-toggle" data-rd-navbar-toggle=".rd-navbar-nav-wrap"><span></span></button>
+                <!-- RD Navbar Brand-->
+                <div class="rd-navbar-brand"><a class="brand-name" href="index.html"><img src="images/CRCazul.png" alt="" width="174" height="32"/></a></div>
+              </div>
+              <!-- RD Navbar Nav-->
+              <div class="rd-navbar-nav-wrap">
+                <ul class="rd-navbar-nav">
+                  <li class="active"><a href="index.html">Inicio</a></li>
+                  
+                    <li><a href="about.html">Nosotros</a></li>
+                    <li><a href="services.html">Servicios</a></li>
+                     <li><a href="contacts.html">Contacto</a></li>
+                    <li><a href="portfolio.html">Portfolio</a>
+                    <li><a href="grid-blog.html">BLOG</a>
+                </ul>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </header>
